364516ba6199e48b2620526362953654d3db397e
Konya Timea <konya.timea2014@gmail.com>
2024-11-30 16:51:28 +0200

    more music

--- secret - Copy/Scripts/player.gd
+++ secret - Copy/Scripts/player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 var near_ladder:=false
 
 @export var can_move=true
+@export var running = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -14,7 +15,9 @@ var near_ladder:=false
 @onready var eye_closed: Sprite2D = $Eye_Closed
 @onready var dust: CPUParticles2D = $CPUParticles2D
 @onready var jump: AudioStreamPlayer = $jump
- 
+@onready var ladder_climb: AudioStreamPlayer = $ladder_climb
+@onready var bush: AudioStreamPlayer = $bush
+@onready var footsteps: AudioStreamPlayer = $footsteps
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor() and !near_ladder:
@@ -37,11 +40,17 @@ func _physics_process(delta: float) -> void:
 			dust.emitting=true
 			dust.gravity=Vector2(400*direction	,0)
 			animation_player.play("run")
+			if not running:
+				footsteps.play()
+			running = true
 			
 			velocity.x = direction * SPEED
 		else:
 			dust.emitting=false
 			animation_player.play("idle")
+			if running:
+				footsteps.stop()
+			running = false
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 		
@@ -57,10 +66,13 @@ func _physics_process(delta: float) -> void:
 
 func _on_surrounding_detector_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Ladder"):
+		ladder_climb.stop()
 		print("I am no longer near a ladder")
 		near_ladder=false
+		
 func _on_surrounding_detector_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Ladder"):
+		ladder_climb.play()
 		print("I am near a ladder")
 		near_ladder=true
 
@@ -68,11 +80,13 @@ func _on_surrounding_detector_body_entered(body: Node2D) -> void:
 func _on_surrounding_detector_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	if area.is_in_group("Bush"):
 		print("I am in the bush")
+		bush.play()
 		visible_for_wolf.set_deferred("monitorable",false)
 		eye_closed.visible=true
 
 func _on_surrounding_detector_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	if area.is_in_group("Bush"):
+		bush.stop()
 		print("I am not in the bush")
 		visible_for_wolf.set_deferred("monitorable",true)
 		eye_closed.visible=false
