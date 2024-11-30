ac8dd5219062259fbbcfb930a1144f6833a92a95
EmmChriss <emmchris@protonmail.com>
2024-11-30 17:41:26 +0200

    working code, main door integration

---
+++ secret - Copy/Scenes/code.gd
@@ -0,0 +1,68 @@
+extends Node2D
+
+@onready var code_tiles_parent = $"Frame/Code"
+@onready var full_symbol = $"FullSymbol"
+
+var symbol_number = 0
+var num_segments = Vector2i(3, 3)
+var skip_segment = Vector2i(0, 0)
+var solution = [null, 0, 1, 2, 3, 4, 5, 6, 7]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setup_code_tiles()
+
+func setup_code_tiles():
+	var texture: Texture2D = load("res://Sprites/symbols/0.png")
+	var s = Vector2i(texture.get_size())
+	var seg_s = s / num_segments
+	
+	var coll_rect = RectangleShape2D.new()
+	coll_rect.size = seg_s
+	
+	# set code tiles manager stuff
+	code_tiles_parent.solution = solution
+	code_tiles_parent.sprites_per_row = 3
+	code_tiles_parent.sprite_size = seg_s
+	
+	# set an offset to center tiles
+	code_tiles_parent.position = -s / 2
+	
+	for seg_x in num_segments.x:
+		for seg_y in num_segments.y:
+			if Vector2i(seg_x, seg_y) == skip_segment:
+				continue
+			
+			var area2d = Area2D.new()
+			area2d.name = "%d_%d".format([seg_x, seg_y])
+			code_tiles_parent.add_child(area2d)
+			
+			# NOTE: sprites are not centered, and that's why we have to offset shapes
+			var collision_shape = CollisionShape2D.new()
+			collision_shape.position = seg_s / 2
+			collision_shape.shape = coll_rect
+			area2d.add_child(collision_shape)
+			
+			var sprite_2d = Sprite2D.new()
+			sprite_2d.texture = texture
+			sprite_2d.region_enabled = true
+			sprite_2d.centered = false
+			
+			var seg_start = Vector2(seg_x, seg_y) * Vector2(seg_s)
+			sprite_2d.region_rect = Rect2(seg_start, seg_s)
+			
+			area2d.add_child(sprite_2d)
+	
+	code_tiles_parent.start()
+	
+	full_symbol.texture = texture
+	code_tiles_parent.connect("solved", puzzle_solved)
+
+func puzzle_solved():
+	full_symbol.visible = true
+	code_tiles_parent.visible = false
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ secret - Copy/Scripts/code_tiles.gd
@@ -0,0 +1,110 @@
+extends Node2D
+
+## called when a solution was found
+## you might want to set enabled to false; to not handle any more click events
+signal solved()
+
+## whether or not to handle clicks on children
+@export var enabled = true
+## speed of animation to new sprite position
+@export var animation_speed: int = 10.0
+## sprites per row in grid
+@export var sprites_per_row: int
+## index is spatial (a slot in the grid); null represents hole; index represents idx-th children in that slot
+@export var solution: Array
+## size of each sprite in world coordinates
+@export var sprite_size: Vector2
+
+# simple list of children, never changed
+var children: Array[Area2D] = []
+
+var handlers = []
+
+# index is spatial (index corresponds with a slot on the grid)
+# value is either null (hole) or index into the child index
+# is copied from solution on _ready, meaning solution has to follow same rules
+var current: Array = []
+
+func get_grid_pos(index: int) -> Vector2i:
+	var y = index / sprites_per_row
+	var x = index % sprites_per_row
+	
+	return Vector2i(x, y)
+
+func get_desired_position(index: int) -> Vector2:
+	return Vector2(get_grid_pos(index)) * sprite_size
+
+func _ready() -> void:
+	start()
+
+func start() -> void:
+	enabled = true
+	
+	# collect children
+	for child in get_children():
+		if child is Area2D:
+			children.append(child)
+	
+	if len(handlers) > 0:
+		for idx in len(children):
+			children[idx].disconnect("input_event", handlers[idx])
+	
+	handlers = []
+	for idx in len(children):
+		handlers.append(func(_vp, evt, _shp): child_clicked(evt, idx))
+		children[idx].connect("input_event", handlers[idx])
+	
+	# fill current position with default values
+	current = solution.duplicate()
+	current.shuffle()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# on every frame, move sprites towards current
+	for idx in len(current):
+		if current[idx] == null:
+			continue
+		
+		# current[idx]-th child moves towards get_desired_position(idx)
+		var child = children[current[idx]]
+		var desired_position = get_desired_position(idx)
+		child.position = child.position.lerp(desired_position, delta * animation_speed)
+
+func child_clicked(event: InputEvent, idx: int):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		pass
+	else:
+		return
+	
+	if idx > len(children):
+		return
+	
+	# find hole
+	var hole_idx = null
+	for i in len(current):
+		if current[i] == null:
+			hole_idx = i
+			break
+	
+	# find child
+	var child_idx = null
+	for i in len(current):
+		if current[i] == idx:
+			child_idx = i
+			break
+	
+	# see if our value is neighboring
+	var child_pos = get_grid_pos(child_idx)
+	var hole_pos = get_grid_pos(hole_idx)
+	
+	if abs(child_pos.distance_to(hole_pos)) - 1 > 0.01:
+		return
+	
+	# swap indexes
+	var child = current[child_idx]
+	current[child_idx] = null
+	current[hole_idx] = child
+	
+	# retarded gdscript by-value array equality test
+	if current.hash() == solution.hash():
+		emit_signal("solved")
